676282d18cdac7ccb01c9d78b63f7dbbc7235f96
higor.andrade@hotmail.com <Higor Souza>
2018-11-11 14:52:53 -0300

    Inserindo comentário na classe SystemServer

--- SistemaBancarioIndependente/src/domain/SystemServer.java
+++ SistemaBancarioIndependente/src/domain/SystemServer.java
@@ -16,4 +16,8 @@ public class SystemServer {
 
 
 
-//http://www.guj.com.br/t/pegando-valor-de-retorno-do-metodo-dentro-de-outro-metodo-resolvido/80853
+
+/**
+ * http://www.guj.com.br/t/pegando-valor-de-retorno-do-metodo-dentro-de-outro-metodo-resolvido/80853
+ * https://www.youtube.com/watch?v=tBiYwv8KHG4 -> Integrar o Git com Eclipse.
+ */
